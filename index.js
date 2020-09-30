f6c0a0a1afc0873f8a0deae54ec5e10fe9f7685f
Abu Taher Molla <57549941+AbuTaherMolla24@users.noreply.github.com>
2020-09-30 19:28:44 +0600

    mod 47 end

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
+const users =['molla', 'kabir', 'sabed','molla','molla','molla',]
+
 app.get('/', (req, res) => {
-    res.send('thank you for calling me');
+    const fruit = {
+        product: 'ada',
+        price: '200'
+    }
+    res.send(fruit);
+
+});
+
+app.get('/users/:id', (req, res) =>{
+    const id = req.params.id;
+    console.log(req.query.sort);
+    const name = users[id];
+    res.send({id, name});
+
+    
 })
 
-app.listen(3000, ()=> console.log('listenig to port 3000'))
+//post
+app.post('/addUser', (req, res) =>{
+    // save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
+})
+
+app.listen(3000, ()=> console.log('listenig to port 3000'));
